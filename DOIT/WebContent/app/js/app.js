65fe22f6747862b8eab63ecfec834fb68c243a10
karthik <karthik.k@hashedin.com>
2014-06-26 19:11:46 +0530

    Landing page with list of projects implemented

--- DOIT/WebContent/app/js/app.js
+++ DOIT/WebContent/app/js/app.js
@@ -1,20 +1,22 @@
 'use strict';
 
-
 // Declare app level module which depends on filters, and services
-angular.module('myApp', [
-  'ngRoute',
-  'myApp.filters',
-  'myApp.services',
-  'myApp.directives',
-  'ui.bootstrap',
-  'myApp.controllers'
-]).
-config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
-  $locationProvider.html5Mode(true);
-  $routeProvider.when('/view1', {templateUrl: 'partials/view1.html', controller: 'MyCtrl1'});
-}]);
+angular.module(
+		'myApp',
+		[ 'ngRoute', 'myApp.filters', 'myApp.services', 'myApp.directives',
+				'ui.bootstrap', 'myApp.controllers' ]).config(
+		[ '$routeProvider', '$locationProvider',
+				function($routeProvider, $locationProvider) {
+//					$locationProvider.html5Mode(true);
+					$routeProvider.when('/createproject', {
+						templateUrl : 'partials/createproject.html',
+						controller : 'CreateProjectController'
+					}).when('/view2', {
+						templateUrl : 'partials/view2.html',
+						controller : 'MyCtrl1'
+					});
+				} ]);
 
 angular.module('myApp').constant('appSettings', {
-  AppURI: 'http://127.0.0.1:9080/'
+	AppURI : 'http://127.0.0.1:9080/'
 });
--- DOIT/WebContent/app/js/controllers.js
+++ DOIT/WebContent/app/js/controllers.js
@@ -2,10 +2,24 @@
 
 /* Controllers */
 
-angular.module('myApp.controllers', [])
-  .controller('MyCtrl1', ['$scope', function($scope) {
-     $scope.message = "Hello This message is from View 1";
-  }])
-  .controller('MyCtrl2', ['$scope', function($scope) {
-    $scope.message = "Hello This message is from View 2";
-  }]);
+angular.module('myApp.controllers', []).controller('CreateProjectController',
+		[ '$scope', function($scope) {
+			$scope.message = "Hello This message is from View 1";
+			$scope.projects = [ {
+				"id" : 1,
+				"name" : "Hashedin HR",
+				"description" : "HR IS HR",
+				"coordinators" : [ "ram", "sham", "boom" ],
+				"isSelect" : "true"
+			},{
+				"id" : 2,
+				"name" : "qwerty ",
+				"description" : "qwerty IS HR",
+				"coordinators" : [ "ram", "karthik" ],
+				"isSelect" : "false"
+			}];
+
+		} ]).controller('ProjectDetailController', [ '$scope', function($scope) {
+	$scope.message = "Hello This message is from View 2";
+	
+} ]);
