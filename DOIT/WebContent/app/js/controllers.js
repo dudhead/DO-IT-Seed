f5e78c8ab474836ece7519defa3f30c8024c562d
karthik <karthik.k@hashedin.com>
2014-06-30 00:03:43 +0530

    working code for fetching list of projects from the service

--- DOIT/WebContent/app/js/controllers.js
+++ DOIT/WebContent/app/js/controllers.js
@@ -1,24 +1,16 @@
 'use strict';
 
 /* Controllers */
+var module = angular.module('myApp.controllers', []);
 
-angular.module('myApp.controllers', []).controller('ListProjectController',
-		[ '$scope', function($scope) {
-			$scope.message = "Hello This message is from View 1";
-			$scope.projects = [ {
-				"id" : 1,
-				"name" : "DO-IT",
-				"description" : "Dummy Description",
-				"coordinators" : [ "Rahul", "Nagarjun" ],
-			}, {
-				"id" : 2,
-				"name" : "DONOT OPEN THIS ",
-				"description" : "qwerty IS HR",
-				"coordinators" : [ "ram", "karthik" ],
-			} ];
 
-		} ]).controller('ProjectDetailController',
-		[ '$scope', '$routeParams', function($scope, $routeParams) {
+module.controller('ListProjectController', function($scope,ProjectsService) {
+	$scope.projects = ProjectsService.list();
+});
+
+
+module.controller('ProjectDetailController', [ '$scope', '$routeParams',
+		function($scope, $routeParams) {
 			$scope.message = "Hello This message is from View 2 ";
 			var projectdetails = [ {
 				"id" : 1,
@@ -44,7 +36,7 @@ angular.module('myApp.controllers', []).controller('ListProjectController',
 				} ]
 
 			} ];
-			
+
 			for (var i = 0; i < projectdetails.length; i++) {
 				var obj = projectdetails[i];
 				if (obj["id"] === parseInt($routeParams.projectId)) {
